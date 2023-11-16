79c9785bb044a9aeb2e3748478a716ff9518b107
ahmed El-Atreby <ahmedsport@hotmail.com>
2023-11-16 10:51:18 +0000

    RouterProvider, createBrowserRouter, createRoutesFromElements, Link, NavLink Outlet, useNavigate and useSearchParams

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,33 +1,36 @@
-
 import { useSelector } from "react-redux";
-import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
+import {
+  selectArticles,
+  filterArticles,
+} from "../features/articles/articlesSlice";
 import Search from "./Search";
 
-import {React, Link} from "react-router-dom"
+import { React, Link, useSearchParams } from "react-router-dom";
 
-export default function Articles () {
+export default function Articles() {
   const articles = useSelector(selectArticles);
 
   // Grab URLSearchParams object from useSearchParams hook
-  
-  // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
+  // Get the queryParams from object returned from useSearchParams and set to `title`
+  const title = searchParams.get("title");
+
+  const filteredArticles = title
+    ? filterArticles(title, articles)
+    : Object.values(articles);
 
   return (
     <main>
       <h1>Articles</h1>
       <ul>
-        { filteredArticles.map(article => (
+        {filteredArticles.map((article) => (
           <li key={article.slug}>
-            <Link to={`${article.slug}`}>
-              {article.title}
-            </Link>
+            <Link to={`${article.slug}`}>{article.title}</Link>
           </li>
         ))}
       </ul>
       <Search />
     </main>
-  )
+  );
 }
